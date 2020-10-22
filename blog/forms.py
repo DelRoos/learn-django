426a051e83f491899bc7d9054379f7ff33f85e88
DelRoos <delanoroosvelt733@gmail.com>
2020-10-22 16:31:20 +0100

    style for login form

--- blog/forms.py
+++ blog/forms.py
@@ -2,5 +2,5 @@ from django import forms
 import django.forms.widgets
 
 class Login(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'required': True}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'required': True}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'required': True, "class": "form-control"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'required': True, "class": "form-control"}))
--- blog/views.py
+++ blog/views.py
@@ -7,13 +7,12 @@ from . import forms
 
 # Create your views here.
 def login(request):
-
     context = {}
     context["form"] = forms.Login
     if request.method == "POST":
         isExist = authenticate(username=request.POST.get("username"), password=request.POST.get("password")) 
         if isExist :
-            context["error"] = "connection is sucessfull"
+            context["error"] = True
         else:
-            context["error"] = "connection is failed"
+            context["error"] = False
     return render(request, "account/login.html", context)
